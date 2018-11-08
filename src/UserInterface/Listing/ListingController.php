347534a78b75e380a93f81ac7f577e4a69b117e3
Jérôme DUMAS <jerome.dumas@travaux.com>
2018-11-08 13:49:42 +0100

    Twig installation

---
+++ src/UserInterface/Listing/ListingController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UserInterface\Listing;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class ListingController
+ * @package App\UserInterface\Listing
+ */
+class ListingController extends  AbstractController
+{
+    /**
+     * @return Response
+     */
+    public function index() : Response
+    {
+        return $this->render(
+            '@Listing/index.html.twig'
+        );
+    }
+}
