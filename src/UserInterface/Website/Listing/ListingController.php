bcefb745f97d297d5dabb7675a5972dd8f9442be
Jérôme DUMAS <jerome.dumas@travaux.com>
2018-11-10 12:01:39 +0100

    Create form for adding listing

--- src/UserInterface/Website/Listing/ListingController.php
+++ src/UserInterface/Website/Listing/ListingController.php
@@ -4,19 +4,23 @@ declare(strict_types=1);
 
 namespace App\UserInterface\Website\Listing;
 
+use App\Core\Listing\Entity\Listing;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class ListingController
  * @package App\UserInterface\Listing
  */
-class ListingController extends  AbstractController
+class ListingController extends AbstractController
 {
     /**
      * @return Response
      */
-    public function index() : Response
+    public function index(): Response
     {
         return $this->render(
             '@Listing/index.html.twig'
@@ -24,12 +28,23 @@ class ListingController extends  AbstractController
     }
 
     /**
+     * @param Request $request
      * @return Response
      */
-    public function create() : Response
+    public function create(Request $request): Response
     {
+        $listing = new Listing();
+
+        $form = $this->createFormBuilder($listing)
+            ->add('name', TextType::class)
+            ->add('Enregistrer', SubmitType::class, array('label' => 'Create Listing'))
+            ->getForm();
+
         return $this->render(
-            '@Listing/create.html.twig'
+            '@Listing/create.html.twig',
+            [
+                'create_form' => $form->createView()
+            ]
         );
     }
 }
