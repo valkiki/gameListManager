df5986a283091523cf97627e701ca3c255d571d1
Jérôme DUMAS <jerome.dumas@travaux.com>
2019-01-11 15:57:42 +0100

    Add addItem mutation in GraphQL API

--- src/Presentation/Api/Graphql/Item/ItemResolver.php
+++ src/Presentation/Api/Graphql/Item/ItemResolver.php
@@ -2,7 +2,9 @@
 
 namespace App\Presentation\Api\Graphql\Item;
 
+use App\Core\Component\Item\Entity\Item;
 use App\Core\Component\Item\Service\ItemService;
+use App\Core\Component\Listing\Service\ListingService;
 use Overblog\GraphQLBundle\Definition\Argument;
 
 class ItemResolver
@@ -11,13 +13,30 @@ class ItemResolver
      * @var ItemService
      */
     private $itemService;
+    /**
+     * @var ListingService
+     */
+    private $listingService;
 
     /**
      * ItemResolver constructor.
      * @param ItemService $itemService
      */
-    public function __construct(ItemService $itemService)
+    public function __construct(ItemService $itemService, ListingService $listingService)
     {
         $this->itemService = $itemService;
+        $this->listingService = $listingService;
+    }
+
+    public function resolveAddItemInListing(Argument $arguments) : void
+    {
+        $listing = $this->listingService->get($arguments['input']['listingId']);
+        $item = new Item();
+        $item->setName($arguments['input']['name']);
+
+        $this->itemService->create(
+            $listing,
+            $item
+        );
     }
 }
